8cbb9a7affca404d068d228072c077b2de6bbfd0
Prachi24599 <54476598+Prachi24599@users.noreply.github.com>
2022-08-16 00:53:23 +0530

    Add files via upload

---
+++ Basic Questions/FibonacciUsingRecursion.java
@@ -0,0 +1,16 @@
+package com.prachi.java;
+
+public class FibonacciUsingRecursion {
+	
+	static int fibo(int n) {
+		if(n <= 1) return n;
+		return fibo(n-1) + fibo(n-2);
+	}
+	
+	public static void main(String args[]) {
+		int n = 10;
+		for(int i = 0; i < n; i++) {
+			System.out.print(fibo(i) + " ");
+		}
+	}
+}
